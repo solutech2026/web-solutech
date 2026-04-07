f0b2161f28352c4ca85082868522cc117a60988c
Herbert Diaz <herbertdiaz63@gmail.com>
2026-04-07 12:00:51 -0400

    cambios

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,7 +8,7 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => title ? `${title} - ${appName}` : appName,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
